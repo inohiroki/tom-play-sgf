9a65c6d1dc2781f97d94911197500d685dbe7537
Hiroki Ino <inohiroki@yahoo.co.jp>
2013-10-13 10:24:26 +0900

    詰碁が動作するよう変更

--- tom-play-sgf.user.js
+++ tom-play-sgf.user.js
@@ -2,6 +2,8 @@
 // @name           tom-play-sgf
 // @namespace      tom
 // @description    棋聖道場のsgfを再生出来るようにする
+// @version        0.0.2
+// @updateURL      https://github.com/inohiroki/tom-play-sgf/raw/master/tom-play-sgf.user.js
 // @include        http://weiqi.sports.tom.com/*
 // @include        http://weiqi.tom.com/*
 // ==/UserScript==
@@ -16,6 +18,10 @@ for (var i = 0; i < links.length; i++) {
 		if(links[i].textContent === "flash版") continue;
 		replace_link(links[i], RegExp.$1);
 	}
+	var link2 = links[i].href.match(/javascript:newwindow2\('(.*)'\)/);
+	if(link2){
+    	replace_link2(links[i], RegExp.$1);
+    }
 }
 
 //
@@ -24,6 +30,23 @@ for (var i = 0; i < links.length; i++) {
 function replace_link(element, link)
 {
 	element.href = "http://ino.xrea.jp/sgf/tom/TomSgfService.php?url=" + link;
-//	element.href = "http://ino.xrea.jp/sgf/playtom.php?url=" + link;
 	element.style.cssText = "border:solid 1px;border-color:#00FF00;";
+}
+
+//
+// 詰碁SGFボタンの追加
+//
+function replace_link2(element, link)
+{
+	var s = link.split("/");
+	var f;
+	if(s.length == 1){ // 古い問題
+		f = s[0].split(".");
+		element.href = "http://ino.xrea.jp/sgf/tom/TomTsumegoService.php?url=http://weiqi.tom.com/hubo/game/" + f[0] + "sgf.htm";
+	}else if(s.length == 2){ // 最近の問題
+		f = s[1].split(".");
+		element.href = "http://ino.xrea.jp/sgf/tom/TomTsumegoService.php?url=http://weiqi.tom.com/hubo/" + s[0] + "/game/" + f[0] + "sgf.htm";
+	}else{
+	}
+	element.childNodes[0].style.cssText = "border:solid 1px;border-color:#00FF00;";
 }
